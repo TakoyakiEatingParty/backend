e9a61c6d8c2f402b86563de22cc7398f52dc7c98
You-Keitou <youkeitou327@gmail.com>
2023-10-06 15:38:06 +0900

    Rubocop rspec導入

--- Magicbox-backend/app/controllers/sessions_controller.rb
+++ Magicbox-backend/app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
 
-	def create
-		user = User.find_or_create_from_auth_hash!(request.env['omniauth.auth'])
-		session[:user_id] = user.id
-		redirect_to 'http://localhost:3001'
-	end
+  def create
+    user = User.find_or_create_from_auth_hash!(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to 'http://localhost:3001'
+  end
 
-	def destroy
-		reset_session
-		render json: { status: :ok, logged_in: false }
-	end
+  def destroy
+    reset_session
+    render json: { status: :ok, logged_in: false }
+  end
 end
--- Magicbox-backend/config/initializers/omniauth.rb
+++ Magicbox-backend/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
+	provider :github, ENV.fetch('GITHUB_CLIENT_ID', nil), ENV.fetch('GITHUB_CLIENT_SECRET', nil)
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get]
