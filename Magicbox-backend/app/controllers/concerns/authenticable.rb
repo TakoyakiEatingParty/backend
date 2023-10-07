7f5f2e302fdc03b629d0144cc0602b1cc940efea
You-Keitou <youkeitou327@gmail.com>
2023-10-08 01:10:02 +0900

    トークン検証機構

--- Magicbox-backend/app/controllers/concerns/authenticable.rb
+++ Magicbox-backend/app/controllers/concerns/authenticable.rb
@@ -1,13 +1,20 @@
-class ApplicationController < ActionController::API
+# app/controllers/concerns/authenticable.rb
+module Authenticable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate_request!
+  end
+
   private
-	def authenticate_request!
+  def authenticate_request!
 	authorization_header = request.headers['Authorization']
 	token = authorization_header.split('Bearer ').last if authorization_header
 	raise 'Token is missing' unless token
 
 	decoded_token = JWT.decode(token, Rails.application.credentials.secret_key_base, true, { algorithm: 'HS256' })
 	@current_user = User.find(decoded_token[0]['user_id'])
-	rescue => e
+  rescue => e
 	render json: { error: e.message }, status: :unauthorized
-	end
+  end
 end
